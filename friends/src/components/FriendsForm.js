3bf22dd172eca81bf54da20e88d2fd74c656c403
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-20 15:43:29 -0700

    Added Friends Form and post function, currently troubleshooting

---
+++ friends/src/components/FriendsForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendsForm extends React.Component {
+
+    state = {
+        friend: {
+            id: null,
+            name: '',
+            age: '',
+            email: '',
+        }
+    };
+
+    handleChange = event => {
+        this.setState({
+            friend: {
+                ...this.state.friend,
+                [event.target.name]: event.target.value
+            }
+        });
+    }
+
+    submit = event => {
+        event.preventDefault();
+        axios
+            .post('http://localhost:5000/api/friends', this.state.friend)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(error => console.log(error.response));
+    }
+
+    render () {
+        return (
+            <div className='login'>
+                <form onSubmit={this.submit}>
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.friend.name}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="age"
+                        value={this.state.friend.age}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        value={this.state.friend.email}
+                        onChange={this.handleChange}
+                    />
+                    <button>Add Friend</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default FriendsForm;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,37 +2,43 @@ import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 import Friend from './Friend';
+import FriendsForm from './FriendsForm';
 
 class FriendsList extends React.Component {
     state = {
         friends: []
     };
 
-    componentDidMount(){
+    componentDidMount() {
         this.getData();
     }
 
     getData = () => {
-       axiosWithAuth()
-        .get('http://localhost:5000/api/friends')
-        .then(res => {
-            console.log(res.data);
-            this.setState({
-                ...this.state,
-                friends: res.data
+        axiosWithAuth()
+            .get('http://localhost:5000/api/friends')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    ...this.state,
+                    friends: res.data
+                })
             })
-        })
-        .catch(error => console.log(error.response)); 
+            .catch(error => console.log(error.response));
     }
 
     render() {
         return (
-            <div className='friends-list'>
-                <h2>Lambda School Friends:</h2>
-                {this.state.friends.map(friend => {
-                    return <Friend key={friend.id} name={friend.name} email={friend.email}/>
-                })}
+            <div className='friends-container'>
+                <div className='friends-list'>
+                    <h2>Lambda School Friends:</h2>
+                    {this.state.friends.map(friend => {
+                        return <Friend key={friend.id} name={friend.name} email={friend.email} />
+                    })}
+                </div>
+                <FriendsForm />
+
             </div>
+
         )
     }
 }
